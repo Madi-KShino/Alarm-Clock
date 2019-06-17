ebdf77f8a382db09c700c6f746d2f750fbba99ad
Madison Kaori Shino <kaorishino@Madisons-MacBook-Pro.local>
2019-06-17 16:22:15 -0600

    Completed Alarm Model

--- AlarmClock/Model/Alarm.swift
+++ AlarmClock/Model/Alarm.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+class Alarm {
+    var fireDate: Date
+    var alarmName: String
+    var alarmEnabled: Bool
+    var uuid: String
+    var fireTimeAsString: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .medium
+        return dateFormatter.string(from: fireDate)
+    }
+    init(fireDate: Date = Date(), alarmName: String, alarmEnabled: Bool, uuid: String) {
+        self.fireDate = fireDate
+        self.alarmName = alarmName
+        self.alarmEnabled = alarmEnabled
+        self.uuid = uuid
+    }
+}
+
+
