f4e7a194860d536cf9ceeb87cb70cc19a9b5a002
Madison Kaori Shino <kaorishino@Madisons-MacBook-Pro.local>
2019-06-17 15:59:11 -0600

    First Commit

---
+++ AlarmClock/View Controller/Custom View/SwitchTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  SwitchTableViewCell.swift
+//  AlarmClock
+//
+//  Created by Madison Kaori Shino on 6/17/19.
+//  Copyright © 2019 Madi S. All rights reserved.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var alarmSwitch: UISwitch!
+    
+    
+    
+    @IBOutlet weak var switchValueChanged: UISwitch!
+    
+}
